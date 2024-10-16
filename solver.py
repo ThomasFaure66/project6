67887c6ecc88d13569ecd7b3ec9c5a7e9bdba96a
melvin <melvin.mathe@student-cs.fr>
2024-10-16 16:40:14 +0200

    added docstring

--- solver.py
+++ solver.py
@@ -1,10 +1,15 @@
 import numpy as np
+from numpy import array
 
-def solve_verlet(R0, F, dt, N, Vi=None):
+def solve_verlet(R0 : array, 
+                 F : callable, 
+                 dt : float, 
+                 N : int, 
+                 Vi=None):
     if Vi is None:
         Vi = np.zeros(R0.shape())
     R = [R0]
-    R1 = R0 + Vi*dt (dt**2)*F(R0)
+    R1 = R0 + Vi*dt (dt**2)*F(R0) # première itération avec potentiellement vitesse aleatoire
     R.append(R1)
 
     for k in range(N-1):

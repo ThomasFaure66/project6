821c31598004ef733de35d554f73d8ed2892e4e2
Faure Thomas <thomas.faure@student-cs.fr>
2024-10-23 14:36:28 +0200

    rajout des codes pour visualtiser la position de H, Cl et la vitesse de Cl au cours du temps

--- main.py
+++ main.py
@@ -37,62 +37,106 @@ def verlet_3d():
     # Listes pour enregistrer les positions au cours du temps
     H_positions = [H_pos.copy()]
     Cl_positions = [Cl_pos.copy()]
+    H_velocity = [H_vel.copy()]
+    Cl_velocity = [Cl_vel.copy()]
     time_list = [0]
     
     #Calcul du premier pas : 
-    F = force(H_pos, Cl_pos)
-
-    H_acc = +F / m_H  # Accélération de H
-    Cl_acc = -F / m_Cl
 
     H_pos_new = H_pos
     Cl_pos_new = Cl_pos 
     H_pos = H_pos_new
     Cl_pos = Cl_pos_new
+    H_vel_new = H_vel
+    Cl_vel_new = Cl_vel
 
     H_positions.append(H_pos.copy())
     Cl_positions.append(Cl_pos.copy())
+    H_velocity.append(H_vel.copy())
+    Cl_velocity.append(Cl_vel.copy())
 
-    for step in range(1, n_steps-1):
+    #Calcul du deuxième pas : 
+
+    F = force(H_pos, Cl_pos)
+    H_acc = F / m_H  # Accélération de H
+    Cl_acc = -F / m_Cl
+
+    H_pos_new = 2*H_pos - H_positions[-2] + H_acc * dt**2
+    Cl_pos_new = 2*Cl_pos - Cl_positions[-2] + Cl_acc * dt**2
+
+    H_pos = H_pos_new
+    Cl_pos = Cl_pos_new
+
+    H_positions.append(H_pos.copy())
+    Cl_positions.append(Cl_pos.copy())
+    H_velocity.append(H_vel.copy())
+    Cl_velocity.append(Cl_vel.copy())
+
+    for step in range(1, n_steps-2):
         # Calcul des forces
         F = force(H_pos, Cl_pos)
         
         # Mise à jour des positions via l'algorithme de Verlet
         H_acc = F / m_H  # Accélération de H
-        Cl_acc = +F / m_Cl  # Accélération de Cl (force opposée)
+        Cl_acc = -F / m_Cl  # Accélération de Cl (force opposée)
         
         # Mise à jour des positions
         H_pos_new = 2*H_pos - H_positions[-2] + H_acc * dt**2
         Cl_pos_new = 2*Cl_pos - Cl_positions[-2] + Cl_acc * dt**2
-        
+        H_vel_new = (3*H_pos - 4*H_positions[-2]+H_positions[-3])/(2*dt)
+        Cl_vel_new = (3*Cl_pos - 4*Cl_positions[-2]+Cl_positions[-3])/(2*dt)
         # Enregistrement des positions et du temps
         H_pos = H_pos_new
         Cl_pos = Cl_pos_new
+        H_vel = H_vel_new
+        Cl_vel = Cl_vel_new
         H_positions.append(H_pos.copy())
         Cl_positions.append(Cl_pos.copy())
+        H_velocity.append(H_vel.copy())
+        Cl_velocity.append(Cl_vel.copy())
         time_list.append(step * dt)
     
-    return time_list, H_positions, Cl_positions
+    return time_list, H_positions, Cl_positions, H_velocity, Cl_velocity
 
 # Lancement de la simulation
 
-time, H_positions, Cl_positions = verlet_3d()
+time, H_positions, Cl_positions, H_velocity, Cl_velocity = verlet_3d()
 # Conversion des résultats pour la visualisation
 H_positions = np.array(H_positions)
 Cl_positions = np.array(Cl_positions)
+H_velocity = np.array(H_velocity)
+Cl_velocity = np.array(Cl_velocity)
 
 
-# Visualisation de la distance entre H et Cl au cours du temps
-distances = [distance(H_positions[i], Cl_positions[i]) for i in range(0,8000)]
-temps = time[0:8000]
-plt.figure(figsize=(10, 6))
-plt.plot(temps, distances)
-plt.axhline(r_eq, color='red', linestyle='--', label='Distance d\'équilibre')
-plt.xlabel('Temps (s)')
-plt.ylabel('Distance H-Cl (m)')
-plt.title('Distance entre H et Cl au cours du temps')
-plt.legend()
-plt.grid(True)
-plt.show()
+# #Visualisation de la distance entre H et Cl au cours du temps
+# distances = [distance(H_positions[i], Cl_positions[i]) for i in range(0,8000)]
+# temps = time[0:8000]
+# plt.figure(figsize=(10, 6))
+# plt.plot(temps, distances)
+# plt.axhline(r_eq, color='red', linestyle='--', label='Distance d\'équilibre')
+# plt.xlabel('Temps (s)')
+# plt.ylabel('Distance H-Cl (m)')
+# plt.title('Distance entre H et Cl au cours du temps')
+# plt.legend()
+# plt.grid(True)
+# plt.show()
 
-fig = plt.figure()
+# #Visualtion de la position sur x de H et de Cl au cours du temps
+# position_H = [H_positions[i][0] for i in range(0,8000)]
+# position_Cl = [Cl_positions[i][0] for i in range(0,8000)]
+# temps = time[0:8000]
+# plt.figure(figsize=(10,6))
+# plt.plot(temps, position_H)
+# plt.plot(temps, position_Cl)
+# plt.xlabel('Temps (s)')
+# plt.grid(True)
+# plt.show()
+
+# #Visualisation de la vitesse sur x de Cl au cours du temps 
+# vitesse_Cl = [Cl_velocity[i][0] for i in range(0,8000)]
+# temps = time[0:8000]
+# plt.figure(figsize=(10,6))
+# plt.plot(temps, vitesse_Cl)
+# plt.xlabel('Temps (s)')
+# plt.grid(True)
+# plt.show()

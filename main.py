3e15998ebf53a092f5dfc7118e8486379b00d23d
Faure Thomas <thomas.faure@student-cs.fr>
2024-10-30 16:17:21 +0100

    Langevin et vitesse initiale

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ gamma = 1e12
 # Paramètres de simulation
 
 dt = 1e-16  # Pas de temps (s)
-n_steps = 80000  # Nombre de pas de temps
+n_steps = 1000000  # Nombre de pas de temps
 
 # Fonction pour calculer la distance entre les deux atomes
 def distance(H_pos, Cl_pos):
@@ -31,31 +31,18 @@ def force(H_pos, Cl_pos):
 def verlet_3d():
     # Initialisation des position
     # s et des vitesses dans l'espace 3D
-    H_pos = np.array([-r_eq/10, 0.0, 0.0])  # Position initiale de H
-    Cl_pos = np.array([r_eq, 0.0, 0.0])  # Position initiale de Cl (à r_eq de H)
+    H_pos = np.array([r_eq, 0.0, 0.0])  # Position initiale de H
+    Cl_pos = np.array([0.0, 0.0, 0.0])  # Position initiale de Cl (à r_eq de H)
     
     H_vel = np.array([0.0, 0.0, 0.0])  # Vitesse initiale de H
     Cl_vel = np.array([0.0, 0.0, 0.0])  # Vitesse initiale de Cl
     
     # Listes pour enregistrer les positions au cours du temps
-    H_positions = [H_pos.copy()]
-    Cl_positions = [Cl_pos.copy()]
-    H_velocity = [H_vel.copy()]
-    Cl_velocity = [Cl_vel.copy()]
+    H_positions = [H_pos, H_pos + H_vel*dt]
+    Cl_positions = [Cl_pos, Cl_pos + Cl_vel*dt]
+    H_velocity = [H_vel, H_vel]
+    Cl_velocity = [Cl_vel, Cl_vel]
     time_list = [0]
-    
-    #Calcul du premier pas : 
-    H_pos_new = H_pos
-    Cl_pos_new = Cl_pos 
-    H_pos = H_pos_new
-    Cl_pos = Cl_pos_new
-    H_vel_new = H_vel
-    Cl_vel_new = Cl_vel
-
-    H_positions.append(H_pos.copy())
-    Cl_positions.append(Cl_pos.copy())
-    H_velocity.append(H_vel.copy())
-    Cl_velocity.append(Cl_vel.copy())
 
     #Calcul du deuxième pas : 
 
@@ -65,11 +52,11 @@ def verlet_3d():
     R_H = np.array([np.sqrt((2*m_H*gamma*kB*T_ther)/dt)*np.random.normal(0, 1),np.sqrt((2*m_H*gamma*kB*T_ther)/dt)*np.random.normal(0, 1),np.sqrt((2*m_H*gamma*kB*T_ther)/dt)*np.random.normal(0, 1)])
     R_Cl = np.array([np.sqrt((2*m_Cl*gamma*kB*T_ther)/dt)*np.random.normal(0, 1),np.sqrt((2*m_H*gamma*kB*T_ther)/dt)*np.random.normal(0, 1),np.sqrt((2*m_H*gamma*kB*T_ther)/dt)*np.random.normal(0, 1)])
 
-    H_acc = F / m_H  - gamma * H_vel_new + R_H/m_H # Accélération de H
-    Cl_acc = -F / m_Cl  - gamma*Cl_vel_new + R_Cl/m_Cl# Accélération de Cl (force opposée)
+    H_acc = F / m_H  - gamma * H_velocity[-1] + R_H/m_H # Accélération de H
+    Cl_acc = -F / m_Cl  - gamma*Cl_velocity[-1] + R_Cl/m_Cl# Accélération de Cl (force opposée)
 
-    H_pos_new = 2*H_pos - H_positions[-2] + H_acc * dt**2
-    Cl_pos_new = 2*Cl_pos - Cl_positions[-2] + Cl_acc * dt**2
+    H_pos_new = 2*H_positions[-1] - H_positions[-2] + H_acc * dt**2
+    Cl_pos_new = 2*Cl_positions[-1] - Cl_positions[-2] + Cl_acc * dt**2
 
 
     H_pos = H_pos_new
@@ -138,14 +125,13 @@ Ep = .5*k*((dist-r_eq)**2)
 Er = Ec-Ev
 
 
-
-# Visualisation de la vitesse sur x de Cl au cours du temps 
+#Visualisation de la vitesse sur x de Cl au cours du temps 
 H = Ec + Ep
-plot1  = [H[i] for i in range(50000)]
-plot2 = [Ep[i] for i in range(50000)]
-plot3 = [Ev[i] for i in range(50000)]
-plot4 = [Er[i] for i in range(50000)]
-temps = time[0:50000]
+plot1  = [H[i] for i in range(900000)]
+plot2 = [Ep[i] for i in range(900000)]
+plot3 = [Ev[i] for i in range(900000)]
+plot4 = [Er[i] for i in range(900000)]
+temps = time[0:900000]
 plt.figure(figsize=(10,6))
 plt.plot(temps, plot1, c="red", label="energie totale")
 plt.plot(temps, plot2, c="blue", label="energie potentielle")
@@ -154,19 +140,6 @@ plt.plot(temps, plot4, c="orange", label="energie rotative")
 plt.legend()
 plt.grid(True)
 plt.show()
-
-# Visualisation de la vitesse sur x de Cl au cours du temps 
-# E = energie_vibratoire + energie_potentielle
-# plot1  = [energie_vibratoire[i] for i in range(70000)]
-# plot2 = [energie_potentielle[i] for i in range(70000)]
-# plot3 = [E[i] for i in range(70000)]
-# temps = time[0:70000]
-# plt.figure(figsize=(10,6))
-# plt.plot(temps, plot1, c="red")
-# plt.plot(temps, plot2, c="blue")
-# plt.plot(temps, plot3, c="green")
-# plt.grid(True)
-# plt.show()
 
 # energie_potentiel = [0.5*k*(distance(H_positions[i], Cl_positions[i])-r_eq)**2 for i in range(0,70000)]
 # mu = (m_H*m_Cl)/(m_Cl+m_H)
@@ -184,8 +157,8 @@ plt.show()
 # plt.show()
 
 #Visualisation de la distance entre H et Cl au cours du temps
-distances = [distance(H_positions[i], Cl_positions[i]) for i in range(0,70000)]
-temps = time[0:70000]
+distances = [distance(H_positions[i], Cl_positions[i]) for i in range(0,900000)]
+temps = time[0:900000]
 plt.figure(figsize=(10, 6))
 plt.plot(temps, distances)
 plt.axhline(r_eq, color='red', linestyle='--', label='Distance d\'équilibre')
